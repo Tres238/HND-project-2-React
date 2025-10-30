b54e020fd19bf1e3dc87a5b1aebf8780001d7363
Tres238 <veratreasure238@gmail.com>
2025-10-30 11:27:14 +0100

    Create Ticket.js

---
+++ Ticket.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Ticket() {
+  return (
+    <section className="tickets">
+      <h2>Your Tickets</h2>
+      <p>List of tickets you have purchased.</p>
+    </section>
+  );
+}
+
+export default Ticket;
