98ef0bf626b7baeb02aa8145de270f511e9edf75
Tres238 <veratreasure238@gmail.com>
2025-10-30 11:18:37 +0100

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="home">
+      <h2>Welcome to TicketEase</h2>
+      <p>Manage all your event tickets with ease!</p>
+    </section>
+  );
+}
+
+export default Home;
