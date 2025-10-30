fef611cab03c1a9619784623aadcab1e061f9863
Tres238 <veratreasure238@gmail.com>
2025-10-30 11:19:43 +0100

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <section className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Overview of your tickets and events.</p>
+    </section>
+  );
+}
+
+export default Dashboard;
